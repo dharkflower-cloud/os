7033eb66b58de4ed1de572f0fafde3deb2e1a464
Taylor Evanson <dharkflower@gmail.com>
2024-07-23 16:40:20 -0600

    yeet

--- scripts/flower.sh
+++ scripts/flower.sh
@@ -39,7 +39,8 @@ npm install -g nodemon pm2 >/dev/null
 
 # VSC Code
 echo "Installing Visual Studio Code..."
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 
+#curl -O https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 rm microsoft.gpg
