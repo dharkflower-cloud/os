56ef4d417c614f7a6f0dcc62e6f7cf4fb8e2216b
Taylor Evanson <dharkflower@gmail.com>
2024-07-25 03:12:05 -0600

    yeet

--- scripts/chroot_build.sh
+++ scripts/chroot_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-set -e                  # exit on error
-set -o pipefail         # exit on pipeline error
+#set -e                  # exit on error
+#set -o pipefail         # exit on pipeline error
 #set -u                  # treat unset variable as error
 #set -x
 
